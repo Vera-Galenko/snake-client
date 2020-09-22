a60179b463f620c4755a8c4dfa0429b8288e0f88
Vera-Galenko <vera.galenko@icloud.com>
2020-09-22 18:07:16 +0000

    setupInput handleUserInput

--- client.js
+++ client.js
@@ -4,15 +4,36 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542,
   });
 // interpret incoming data as text
 conn.setEncoding('utf8'); 
- conn.on('connect', () => {
+  conn.on('connect', () => {
     conn.write('Name: VG');
     console.log("Successfully connected to game server" );
+  })
+
+  conn.on('connect', () => {
+    conn.write("Move: up");
+    console.log("Successfully sent the command up" );
+  })
+  conn.on('connect', () => {
+    conn.write("Move: down");
+    console.log("Successfully sent the command down" );
+  })
+  conn.on('connect', () => {
+    conn.write("Move: left");
+    console.log("Successfully sent the command left" );
+  })
+  conn.on('connect', () => {
+    conn.write("Move: left");
+    console.log("Successfully sent the command left" );
+  })
+  conn.on('connect', () => {
+    conn.write("Move: right");
+    console.log("Successfully sent the command right" );
   })
 
   conn.on('data', data => { //get message back from server
@@ -25,3 +46,5 @@ conn.setEncoding('utf8');
 // console.log('Connecting ...');
 // connect();
 // // setupInput(connection);
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = function(data){
+    if (data === '\u0003') {
+        console.log('Game over')
+        process.exit();
+      }   
+}
+
+const setupInput = function() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    return stdin;
+  }
+
