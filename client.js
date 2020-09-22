dc313c79e8b3107e321c06ab4b502132527b256b
Vera-Galenko <vera.galenko@icloud.com>
2020-09-22 18:29:24 +0000

    movement commands

--- client.js
+++ client.js
@@ -15,26 +15,7 @@ conn.setEncoding('utf8');
     console.log("Successfully connected to game server" );
   })
 
-  conn.on('connect', () => {
-    conn.write("Move: up");
-    console.log("Successfully sent the command up" );
-  })
-  conn.on('connect', () => {
-    conn.write("Move: down");
-    console.log("Successfully sent the command down" );
-  })
-  conn.on('connect', () => {
-    conn.write("Move: left");
-    console.log("Successfully sent the command left" );
-  })
-  conn.on('connect', () => {
-    conn.write("Move: left");
-    console.log("Successfully sent the command left" );
-  })
-  conn.on('connect', () => {
-    conn.write("Move: right");
-    console.log("Successfully sent the command right" );
-  })
+
 
   conn.on('data', data => { //get message back from server
     console.log('server message', data)
--- input.js
+++ input.js
@@ -1,12 +1,33 @@
+let connection;
+
 const handleUserInput = function(data){
     if (data === '\u0003') {
         console.log('Game over');
         process.exit();
       }   
-    if (key === )
+    if (key === 'w'){
+        connection.write("Move: up");
+        console.log("Successfully sent the command up" );
+    }
+    if (key === 'a'){
+        connection.write("Move: left");
+        console.log("Successfully sent the command left" );
+    }
+    if (key === 's'){
+        connection.write("Move: down");
+        console.log("Successfully sent the command down" );
+    }
+    if (key === 'd'){
+        connection.write("Move: right");
+        console.log("Successfully sent the command right" );
+    }
+
+
+
 }
 
 const setupInput = function() {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding('utf8');
